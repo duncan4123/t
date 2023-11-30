eb3838dc600412582be9f9e3843088757df59499
dunks411 <54425677+duncan4123@users.noreply.github.com>
2023-11-30 17:10:21 +1100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Telegraf } from 'telegraf'
+
+const BOT_TOKEN = process.env.BOT_TOKEN!
+export const bot = new Telegraf(BOT_TOKEN)
+
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Help text...'))
